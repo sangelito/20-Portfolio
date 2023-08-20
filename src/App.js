dc1cbf2e762490dd1b6c241ab036096845da1bc9
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-20 13:47:47 -0500

    imports

---
+++ src/App.js
@@ -0,0 +1,8 @@
+//will try to import logo here [still need to create logo]
+import "./App.css";
+//import main page from ./pages [still need to create pages]
+import Nav from "./components/Navigation";
+import AboutMe from "../src/pages/AboutMe";
+import Contact from "../src/pages/Contact";
+import React, { useState } from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
