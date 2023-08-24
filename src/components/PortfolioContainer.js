69ec1e92c65dcfedc11ab0fe11d86f265c09cd38
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 15:54:14 -0500

    moved from portfolio

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -4,3 +4,28 @@ import Portfolio from "../pages/Portfolio";
 import Resume from "../pages/Resume";
 import AboutMe from "../pages/AboutMe";
 import Contact from "../pages/Contact";
+
+function PortfolioContainer({ currentPage }) {
+    // const renderPage = () => {
+    function renderPage() {
+      switch (currentPage.name) {
+        case "about me":
+          return <AboutMe />;
+        case "portfolio":
+          return <Portfolio />;
+        case "contact":
+          return <Contact />;
+        case "resume":
+          return <Resume />;
+        default:
+          return <AboutMe />;
+      }
+    }
+
+    return (
+        <div>
+          <div>{renderPage()}</div>
+        </div>
+      );
+}
+export default PortfolioContainer;
