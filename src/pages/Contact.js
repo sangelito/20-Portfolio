c0f9b35e24a63228398348c11578dabd779cea88
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 14:53:21 -0500

    imports

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
+import {
+  validateEmail,
+  validateUserName,
+  validateMessage,
+} from "../utils/helpers";
+// Importing a helper function
 
+import {
+  MDBInput,
+  MDBRow,
+  MDBCol,
+  MDBTextArea,
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import "./pages.css";
