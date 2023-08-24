43a5c48cef600ebbc6ff6ffa9ea385ab717a8caa
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 13:26:21 -0500

    import react

---
+++ src/pages/AboutMe.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../pages";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardHeader,
+  MDBCardFooter,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React, { useState } from "react";
-import {
-  validateEmail,
-  validateUserName,
-  validateMessage,
-} from "../utils/helpers";
