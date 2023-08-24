3b2158ff50972d443b5861aefb8922d391ef8ce8
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 15:04:51 -0500

    render page according to user click

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,3 +2,21 @@ import React, { useState } from "react";
 import Nav from "./Navigation";
 import AboutMe from "../pages/AboutMe";
 import Contact from "../pages/Contact";
+
+function PortfolioContainer({ currentPage }) {
+    // const renderPage = () => {
+    function renderPage() {
+      switch (currentPage.name) {
+        case "about me":
+          return <AboutMe />;
+        case "portfolio":
+          return <Portfolio />;
+        case "contact":
+          return <Contact />;
+        case "resume":
+          return <Resume />;
+        default:
+          return <AboutMe />;
+      }
+    }
+}
