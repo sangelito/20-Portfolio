5038d5560a6dbb37e358f312da95c8ac4483e696
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 15:16:13 -0500

    nav bar details

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Nav({ setCurrentPage, currentPage, pages }) {
-
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
+            <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarExample01"
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+          </div>
+            </nav>
+        </header>
+    );
 }
 
 export default Nav;
