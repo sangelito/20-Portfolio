6f66df1036520661eff1220679ac4479cdac5ade
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 14:41:55 -0500

    About me info

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -14,5 +14,42 @@ import { Link } from "react-router-dom";
 import ProfilePic from "../assets/steph.jpg";
 
 export default function AboutMe() {
+    return (
+        <div>
+          <div>
+            <div className="aboutMe aboutMeContainer">
+              <img
+                src={ProfilePic}
+                className="profile-picture"
+                alt="profile"
+                style={{
+                  paddingTop: "35px",
+                }}
+              />
+              <p
+                className="aboutMe text"
+                style={{
+                  paddingTop: "35px",
+                  fontSize: "20px",
+                }}
+              >
+               Hello! I'm Stephanie an aspiring software developer 
+                <br />
+                Email me at laurakam91@gmail.com
+                <br />
+                <span>
+                  <a href="https://github.com/sangelito">GitHub {"   "} </a>
+                </span>
+                <span>
+                  <a href="https://www.linkedin.com/in/stephanie-angelito-33816322b/">
+                    LinkedIn {"   "}
+                  </a>
+                </span>
+                <br />
+              </p>
+            </div>
+          </div>
+        </div>
+      );
 }
           
