8de9a093c68c4cafb99a3f532dccfb176887b040
sangelito <129316135+sangelito@users.noreply.github.com>
2023-08-24 19:09:34 -0500

    completed card

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,15 +2,16 @@ import React from "react";
 import { MDBBadge } from "mdb-react-ui-kit";
 import "./pages.css";
 
+import weatherdashboard from "../assets/Weather Dashboard.png";
 export default function Portfolio() {
     return (
         <div class="row row-cols-1 row-cols-sm-3 g-4">
             <div className="col" >
             <img
             className="imgScreenshot"
-            src={}
+            src={weatherdashboard}
             class="card-img-top"
-            alt=""
+            alt="Weather Dashboard"
           />
             </div>
         </div>
